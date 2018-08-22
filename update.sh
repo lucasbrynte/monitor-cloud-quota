e2a960461f2c326c01cdc281f785702932b53a87
Lucas Brynte <brynte@chalmers.se>
2018-08-22 15:27:29 +0200

    update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+# Pull latest release of monitor-cloud-quota tool
+git fetch && git reset --hard origin/latest-release
+
+# # Install any additional system requirements
+# sudo apt-get update
+# while read -r line; do sudo apt-get -y install "$line"; done < ubuntu-deps.txt
+# 
+# # Install any additional python requirements
+# pip install -r python-deps.txt
