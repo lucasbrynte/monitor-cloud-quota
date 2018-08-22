ff02a1edd3c8b7a166b72dda15e8343339341d5b
Lucas Brynte <brynte@chalmers.se>
2018-08-22 16:03:45 +0200

    only shutdown if feature activated

--- check_quota.py
+++ check_quota.py
@@ -7,5 +7,7 @@ uptime_hours = misc.get_uptime_seconds() / 3600.
 print('{:.2f} / {} hours consumed.'.format(uptime_hours, conf['budget_hours']))
 
 if uptime_hours > conf['budget_hours']:
-    print('Out of budget - shutting down machine.')
-    # os.system('sudo poweroff')
+    if os.path.exists(conf['toggle_shutdown_file']):
+        print('Out of budget! Shutting down machine.')
+        os.remove(conf['toggle_shutdown_file'])
+        os.system('sudo poweroff')
